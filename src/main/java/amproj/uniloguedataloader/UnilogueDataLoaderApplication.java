234439cca0770c211d7d2451b910bbc0af3a7342
AntonM-248 <atnmku@gmail.com>
2022-01-27 11:36:00 -0500

    Added spring initializer files and entered secure connect into gitignore

---
+++ src/main/java/amproj/uniloguedataloader/UnilogueDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package amproj.uniloguedataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnilogueDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnilogueDataLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/amproj/uniloguedataloader/UnilogueDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package amproj.uniloguedataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnilogueDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
